1f620e5caff6c522313eafab03cb50d70aeb8276
Aliaksei Syrel <alex.syrel@gmail.com>
2021-06-25 22:25:33 +0200

    if we build libgit2 on unix, copy the binaries and symlink them

--- cmake/importLibGit2.cmake
+++ cmake/importLibGit2.cmake
@@ -72,8 +72,15 @@ function(build_git2)
 			COMMAND ${CMAKE_COMMAND} -E create_symlink ${LIBRARY_OUTPUT_DIRECTORY}/libgit2.1.0.1.dylib ${LIBRARY_OUTPUT_DIRECTORY}/libgit2.dylib
 			COMMAND ${CMAKE_COMMAND} -E create_symlink ${LIBRARY_OUTPUT_DIRECTORY}/libgit2.1.0.1.dylib ${LIBRARY_OUTPUT_DIRECTORY}/libgit2.1.0.0.dylib
 			COMMENT "Copying Libgit binaries from '${LibGit2_BINARY_DIR}' to '${LIBRARY_OUTPUT_DIRECTORY}'" VERBATIM)
-	else()
-		message(FATAL "Aggggh not implemented yet")
+	elseif(UNIX)
+		add_custom_target(libgit2_copy
+        	COMMAND ${CMAKE_COMMAND} -E copy_if_different ${LibGit2_BINARY_DIR}/libgit2.1.0.1.so ${LIBRARY_OUTPUT_DIRECTORY}
+        	COMMAND ${CMAKE_COMMAND} -E create_symlink ${LIBRARY_OUTPUT_DIRECTORY}/libgit2.1.0.1.so ${LIBRARY_OUTPUT_DIRECTORY}/libgit2.1.0.so
+            COMMAND ${CMAKE_COMMAND} -E create_symlink ${LIBRARY_OUTPUT_DIRECTORY}/libgit2.1.0.1.so ${LIBRARY_OUTPUT_DIRECTORY}/libgit2.so
+            COMMAND ${CMAKE_COMMAND} -E create_symlink ${LIBRARY_OUTPUT_DIRECTORY}/libgit2.1.0.1.so ${LIBRARY_OUTPUT_DIRECTORY}/libgit2.1.0.0.so
+        	COMMENT "Copying Libgit binaries from '${LibGit2_BINARY_DIR}' to '${LIBRARY_OUTPUT_DIRECTORY}'" VERBATIM)
+    else()
+        message(FATAL "Aggggh not implemented yet")
 	endif()
 
 	add_dependencies(libgit2_copy git2)
